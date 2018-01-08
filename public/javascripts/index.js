bb9e2b8fcec0bce78a49fe474cf0997c827c861e
Utarwyn <maxime.malgorn@laposte.net>
2018-01-08 16:30:39 +0100

    Mise à jour page d'accueil

---
+++ public/javascripts/index.js
@@ -0,0 +1,29 @@
+window.addEventListener("load", function() {
+
+    var BASE_DELAY = 3000;
+
+    var h1  = document.querySelector("h1.main");
+    var trs = h1.querySelectorAll("span");
+    var idx = -1;
+
+    titreReroll();
+
+    function titreReroll() {
+        if (idx >= 0)
+            trs[idx].classList.remove("echo");
+
+        trs[++idx].classList.add("echo");
+
+        if (idx < trs.length - 1)
+            setTimeout(titreReroll, (idx>0)?(BASE_DELAY/idx):BASE_DELAY);
+    }
+
+    Particles.init({
+        selector: '.particles',
+        color: '#555555',
+        maxParticles: 30,
+        connectParticles: true,
+        speed: 0.6
+    });
+
+});
